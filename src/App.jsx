7b806f7a3639f733ba246590ce3638b5a65578b8
celesteag <celeste.ag.2c@gmail.com>
2026-02-19 19:36:31 +0000

    footer content

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Contact from './pages/contact/Contact'
+import Privacy from "./components/privacy/Privacy";
+import Cookies from "./components/cookies/Cookies";
+import Terms from "./components/terms/Terms";
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/cookies" element={<Cookies />} />
+        <Route path="/terms" element={<Terms />} />
       </Routes>
       <Footer />
     </BrowserRouter>
