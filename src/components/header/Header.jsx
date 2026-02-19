e5de8d34863549e839a900076e25838c51f08a94
celesteag <celeste.ag.2c@gmail.com>
2026-02-19 16:28:13 +0000

    header changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,13 +7,16 @@ function Header() {
     <header className="header">
       <div className="header-container">
 
+        <nav className="header-nav left">
+          <NavLink to="/" className="nav-link">Home</NavLink>
+          <NavLink to="/about" className="nav-link">About</NavLink>
+        </nav>
+
         <NavLink to="/" className="header-logo">
           <img src={logo} alt="TravelWeb logo" />
         </NavLink>
 
-        <nav className="header-nav">
-          <NavLink to="/" className="nav-link">Home</NavLink>
-          <NavLink to="/about" className="nav-link">About</NavLink>
+        <nav className="header-nav right">
           <NavLink to="/contact" className="nav-link">Contact</NavLink>
         </nav>
 
