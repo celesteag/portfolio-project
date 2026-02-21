65cf90566cec0e88c3ef219dface613b6caccce4
Celeste <celeste.ag.2c@gmail.com>
2026-02-21 13:34:50 +0100

    fix url

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,14 @@
 const projects = [
   {
-    id: 1,
+    id: 0,
     title: "Nombre del repo",
     description: "Descripción de lo que hace el proyecto, tecnologías usadas, etc.",
     repo: "https://github.com/tuusuario/tu-repo",
+    demo: "https://celesteag.github.io/Final-Project/",
     tags: ["React", "CSS"],
   },
   {
-    id: 2,
+    id: 1,
     title: "Otro proyecto",
     description: "Otra descripción chula.",
     repo: "https://github.com/tuusuario/otro-repo",
