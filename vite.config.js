360753fef09358904cb6a7465cb143590c0fe9fe
Celeste <celeste.ag.2c@gmail.com>
2026-02-22 09:49:00 +0100

    fix base url portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RobEurope/',
+  base: '/portfolio-project/',
   //  base: process.env.NODE_ENV === 'production' ? '/RobEurope/' : '/'
 })
 
